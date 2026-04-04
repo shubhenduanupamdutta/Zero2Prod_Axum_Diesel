6094821332df0071d2d63e1a65ce2015cf97a073
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-04-04 19:08:28 +0530

    Refactor code into lib and main for easier testing

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use axum::{Router, http::StatusCode, response::IntoResponse, routing::get};
+use tokio::net::TcpListener;
+
+
+async fn health_check() -> impl IntoResponse {
+    StatusCode::OK
+}
+
+
+pub async fn run() -> Result<(), std::io::Error> {
+    let app = Router::new().route("/health_check", get(health_check));
+    let listener = TcpListener::bind("127.0.0.1:8000").await?;
+    axum::serve(listener, app).await
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
-use axum::{Router, http::StatusCode, response::IntoResponse, routing::get};
-use tokio::net::TcpListener;
-
-
-async fn health_check() -> impl IntoResponse {
-    StatusCode::OK
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let app = Router::new().route("/health_check", get(health_check));
-    let listener = TcpListener::bind("127.0.0.1:8000").await?;
-    axum::serve(listener, app).await
+    run().await
 }
