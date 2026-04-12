44ff24c90e17f451a83efa716014daea8865f7ab
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-04-12 18:29:13 +0530

    Create subscriptions table migration

---
+++ migrations/2026-04-12-124918-0000_create_subscriptions_table/up.sql
@@ -0,0 +1,8 @@
+-- Create subscriptions table
+CREATE TABLE subscriptions (
+    id uuid NOT NULL,
+    PRIMARY KEY (id),
+    email TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL,
+    subscribed_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
