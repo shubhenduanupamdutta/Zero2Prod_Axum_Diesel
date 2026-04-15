9127cb3e0c8e31916fced9b27d02307d6c6411f2
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-04-15 19:05:17 +0530

    Add async Postgres connection to app state
    Signed-off-by: Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use diesel_async::{AsyncConnection, AsyncPgConnection};
 use tokio::net::TcpListener;
 use zero2prod::{configuration::get_configuration, startup::run};
 
@@ -5,8 +6,11 @@ use zero2prod::{configuration::get_configuration, startup::run};
 async fn main() -> Result<(), std::io::Error> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = AsyncPgConnection::establish(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).await?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,28 @@
+use std::sync::Arc;
+
 use axum::{
     Router,
     routing::{get, post},
     serve::Serve,
 };
+use diesel_async::AsyncPgConnection;
 use tokio::net::TcpListener;
 
 use crate::routes::{health_check, subscribe};
 
 
-pub fn run(listener: TcpListener) -> Result<Serve<TcpListener, Router, Router>, std::io::Error> {
+pub fn run(
+    listener: TcpListener,
+    connection: AsyncPgConnection,
+) -> Result<Serve<TcpListener, Router, Router>, std::io::Error> {
+    // Wrap the connection in an `Arc` to share it across multiple handlers
+    let connection = Arc::new(connection);
+
     let app = Router::new()
         .route("/health_check", get(health_check))
-        .route("/subscriptions", post(subscribe));
+        .route("/subscriptions", post(subscribe))
+        // Add the connection to the application state so it can be accessed in handlers
+        .with_state(connection);
     let server = axum::serve(listener, app);
     Ok(server)
 }
