22b10dd1df5b2f56aa95ade44bc08efb016e1f47
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-04-25 22:27:39 +0530

    Use tracing with Bunyan JSON formatter
    Replace env_logger with tracing-subscriber and tracing-bunyan-formatter,
    forward log crate records via tracing-log/LogTracer, and add an
    EnvFilter.
    Configure a layered JSON subscriber as the global default and instrument
    the subscription handler with spans, using .instrument for the DB query.
    
    Signed-off-by: Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,31 @@ use diesel_async::{
     AsyncPgConnection,
     pooled_connection::{AsyncDieselConnectionManager, deadpool::Pool},
 };
-use env_logger::Env;
 use tokio::net::TcpListener;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // `init` does call `set_logger`, so this is all we need to do.
-    // We are falling back to printing all logs at info-level or above
-    // if the RUST_LOG environment variable has not been set.
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // Redirect all `log`'s events to our subscriber
+    LogTracer::init().expect("Failed to set logger");
+
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info,tower_http=debug"));
+    let formatting_layer = BunyanFormattingLayer::new(
+        "zero2prod".into(),
+        // Output the formatted span to stdout
+        std::io::stdout,
+    );
+
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to get subscriber");
 
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,6 +3,7 @@ use chrono::{DateTime, Utc};
 use diesel::prelude::Insertable;
 use diesel_async::RunQueryDsl;
 use serde::Deserialize;
+use tracing::Instrument;
 use uuid::Uuid;
 
 use crate::{DbPool, schema::subscriptions};
@@ -26,12 +27,20 @@ struct InsertSubscription {
 
 pub async fn subscribe(State(pool): State<DbPool>, Form(form): Form<FormData>) -> StatusCode {
     let request_id = Uuid::new_v4();
-    log::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
+
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name,
     );
+
+    let _request_span_guard = request_span.enter();
+
+    // We do not call `.enter` on query_span!
+    // `.instrument` takes care of it at the right moments
+    // in the query lifetime
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
 
     let new_subscriber = InsertSubscription {
         id: Uuid::new_v4(),
@@ -45,28 +54,17 @@ pub async fn subscribe(State(pool): State<DbPool>, Form(form): Form<FormData>) -
         .await
         .expect("Failed to get a connection from the pool.");
 
-    log::info!(
-        "request_id {} - Saving new subscriber details in the database",
-        request_id
-    );
     match diesel::insert_into(subscriptions::table)
         .values(&new_subscriber)
         .execute(&mut conn)
+        .instrument(query_span)
         .await
     {
-        Ok(_) => {
-            log::info!(
-                "request_id {} - New subscriber details have been saved successfully.",
-                request_id
-            );
-            StatusCode::OK
-        },
+        Ok(_) => StatusCode::OK,
         Err(e) => {
-            log::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
+            // Yes, this error log falls outside of `query_span`
+            // We'll rectify it later, pinky swear!
+            tracing::error!("Failed to execute query: {:?}", e);
             StatusCode::INTERNAL_SERVER_ERROR
         },
     }
