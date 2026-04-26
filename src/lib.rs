92a9750831e667d3ccd72fc7fec336e723287bb2
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-04-26 11:17:53 +0530

    Move tracing setup to telemetry module
    Signed-off-by: Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,6 @@ pub mod configuration;
 pub mod routes;
 pub mod schema;
 pub mod startup;
+pub mod telemetry;
 
 pub type DbPool = deadpool::Pool<AsyncPgConnection>;
--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,17 @@ use diesel_async::{
     pooled_connection::{AsyncDieselConnectionManager, deadpool::Pool},
 };
 use tokio::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
-use zero2prod::{configuration::get_configuration, startup::run};
+use zero2prod::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
+
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // Redirect all `log`'s events to our subscriber
-    LogTracer::init().expect("Failed to set logger");
-
-    let env_filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new("info,tower_http=debug"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "zero2prod".into(),
-        // Output the formatted span to stdout
-        std::io::stdout,
-    );
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to get subscriber");
+    let subscriber = get_subscriber("zero2prod".into(), "info,http_tower=debug".into());
+    init_subscriber(subscriber);
 
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
